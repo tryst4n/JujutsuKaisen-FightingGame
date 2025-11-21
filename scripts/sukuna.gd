15138d553603a3e4dd47536a001110478507bf77
trystan <trystankhang@gmail.com>
2025-11-21 04:10:48 -0500

    FUNC Attack animation + hitbox -made an attack animation (not polished) -added hitbox to the attack -no attacks during jump + attack stops movement

--- scripts/sukuna.gd
+++ scripts/sukuna.gd
@@ -5,9 +5,12 @@ const SPEED = 150.0
 var ACCELERATION := 300
 const JUMP_VELOCITY = -400.0
 @onready var sprite = $Sprite2D
+@onready var spriteForWeapons = $Sprite2D_SpearAttack
 @onready var anim_tree : AnimationTree =  $AnimationTree
 @onready var sukuna = $"."
 var dismantle_scene_path=preload("res://scenes/dismantle.tscn")
+var is_attacking = false
+var is_jumping = !(velocity.y == 0) #if velocity in y != 0, character is jumping
 
 func _ready():
 	anim_tree.active = true
@@ -32,12 +35,44 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# PREVENT MOVEMENT WHILE ATTACKING
+	if is_attacking:
+		velocity.x = 0 # freeze left/right
+		velocity.y = move_toward(velocity.y, 0, 9999) #freeze jump
+		move_and_slide()
+		return
 		
 	#Shooting Dismantle
-	if Input.is_action_just_pressed("main_weapon") :
+	if Input.is_action_just_pressed("main_ability") :
 		shootDismantle()
-
+		
+	#Attack
+	if Input.is_action_just_pressed("attack") and is_on_floor() and !is_attacking and !Input.is_key_pressed(KEY_SHIFT):#prevents attacking if it already is attacking #we make sure shift is not pressed because if not, this will also trigger when main_ability is going off since we're using left click for both
+		attack()
 	move_and_slide()
+	
+func attack():
+	is_attacking = true
+	sprite.visible = false
+	spriteForWeapons.visible = true
+	
+	#get mosue position
+	var mouse_pos = get_global_mouse_position()
+	#computes a vector between player and the mouse and convert it to an angle in radians
+	var direction = (mouse_pos - global_position).angle()
+	
+	#Flip sukuna to the cursor's side
+	# convert radians to degrees
+	var angle_deg = rad_to_deg(direction)
+	if angle_deg > 90 or angle_deg < -90:
+		sprite.flip_h = true
+		spriteForWeapons.flip_h = true
+	else:
+		sprite.flip_h = false
+		spriteForWeapons.flip_h = false
+	
+	await get_tree().create_timer(0.8).timeout
+	is_attacking = false
 	
 func shootDismantle():
 	var dismantle=dismantle_scene_path.instantiate()
@@ -54,8 +89,10 @@ func shootDismantle():
 	var angle_deg = rad_to_deg(direction)
 	if angle_deg > 90 or angle_deg < -90:
 		sprite.flip_h = true
+		spriteForWeapons.flip_h = true
 	else:
 		sprite.flip_h = false
+		spriteForWeapons.flip_h = false
 	
 	dismantle.dir=direction # the projectile's movement direction in radians
 	dismantle.pos=sukuna.global_position + spawn_offset# starting position of projectile + the offset
@@ -65,30 +102,49 @@ func shootDismantle():
 	
 func update_animation_parameters():
 	var direction := Input.get_axis("move_left", "move_right")
-	var is_jumping = !(velocity.y == 0) #if velocity in y = 0, character is jumping
 	#flip the sprite depending on facing direction
 	if direction > 0 : 
 		sprite.flip_h = false
+		spriteForWeapons.flip_h = false
 	elif direction < 0 :
 		sprite.flip_h = true
-	
+		spriteForWeapons.flip_h = true
+	#ATTACKING
+	if is_attacking:
+		sprite.visible = false
+		spriteForWeapons.visible = true
+		anim_tree["parameters/conditions/idle"] = false
+		anim_tree["parameters/conditions/start_run"] = false
+		anim_tree["parameters/conditions/is_running"] = false
+		anim_tree["parameters/conditions/is_jumping"] = false
+		anim_tree["parameters/conditions/attack"] = true
+		return
 	#HANDLE JUMPING
 	if is_jumping :
+		sprite.visible = true
+		spriteForWeapons.visible = false
 		anim_tree["parameters/conditions/idle"] = false
 		anim_tree["parameters/conditions/start_run"] = false
 		anim_tree["parameters/conditions/is_running"] = false
 		anim_tree["parameters/conditions/is_jumping"] = true
+		anim_tree["parameters/conditions/attack"] = false
 		return
 	anim_tree["parameters/conditions/is_jumping"] = false
 	
 	#HANDLE IDLE / RUN
 	if direction == 0 :
+		sprite.visible = true
+		spriteForWeapons.visible = false
 		anim_tree["parameters/conditions/idle"] = true
 		anim_tree["parameters/conditions/start_run"] = false
 		anim_tree["parameters/conditions/is_running"] = false
+		anim_tree["parameters/conditions/attack"] = false
 	else:
+		sprite.visible = true
+		spriteForWeapons.visible = false
 		anim_tree["parameters/conditions/idle"] = false
 		anim_tree["parameters/conditions/start_run"] = true
 		#when start_run animation will finish, run animation will start as set in the AnimationTree StateMachine
 		anim_tree["parameters/conditions/is_running"] = true
+		anim_tree["parameters/conditions/attack"] = false
 		
